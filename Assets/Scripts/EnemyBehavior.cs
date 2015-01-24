993c46bb8d29f38e0584e37d6fd8eac5792b1330
Michael John Yoder <mjyoder@un004-imini17.local>
2015-01-24 10:22:36 -0500

    Did the thing.

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class EnemyBehavior : MonoBehaviour {
 	public float maxSpeed = 3.0f;
 	public GameObject playerPtr;
+	public int health = 100;
 	// Use this for initialization
 	void Start () {
 		
@@ -13,11 +14,22 @@ public class EnemyBehavior : MonoBehaviour {
 		Debug.Log (transform.childCount);
 		var step = maxSpeed * Time.deltaTime;
 		transform.position = Vector2.MoveTowards (transform.position, playerPtr.transform.position, step);
+		FaceCharacter ();
 
 	}
 
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
+
+	void FaceCharacter()
+	{
+				var otherPos = playerPtr.transform.position;
+				var pos = transform.position;
+				var dir = otherPos - pos;
+				var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+				transform.rotation = Quaternion.AngleAxis (angle, Vector3.forward); 
+		}
+
 }

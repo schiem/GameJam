c55f7bd3993a202e13574836e68864b9689adeda
Michael John Yoder <mjyoder@un004-imini17.local>
2015-01-23 20:31:20 -0500

    Added a maincharacter controller.

---
+++ Assets/KeyboardController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyboardController : MonoBehaviour {
+	public float speed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void FixedUpdate(){
+		float y_val = Input.GetAxis ("Vertical") * speed;
+		float x_val = Input.GetAxis ("Horizontal") * speed;
+
+		transform.Translate (x_val, y_val, 0);
+	
+	}
+
+}

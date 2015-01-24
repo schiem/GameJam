447edf870eaf04ee6dd862c0d15708e9cc23e125
System Administrator <root@un004-imini16.local>
2015-01-23 20:22:01 -0500

    YOLO

---
+++ Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehavior : MonoBehaviour {
+	public float maxSpeed = 3.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void FixedUpdate () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

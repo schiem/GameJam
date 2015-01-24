7d7e9b3494e775dc56e5fd8f06683a7eee02d18c
Seth Aaron Yoder <sayoder@un004-imini16.local>
2015-01-23 20:45:27 -0500

    commit to merge

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -3,13 +3,16 @@ using System.Collections;
 
 public class EnemyBehavior : MonoBehaviour {
 	public float maxSpeed = 3.0f;
+	public GameObject playerPtr;
 	// Use this for initialization
 	void Start () {
 		
 	}
 
 	void FixedUpdate () {
-
+		Debug.Log (transform.childCount);
+		var step = maxSpeed * Time.deltaTime;
+		transform.position = Vector2.MoveTowards (transform.position, playerPtr.transform.position, step);
 	}
 
 	// Update is called once per frame
